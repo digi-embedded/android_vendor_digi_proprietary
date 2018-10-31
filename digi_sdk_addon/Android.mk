9937ace1f12e907f2b540a9c8050d5f45e341444
Tatiana Leon <tatiana.leon@digi.com>
2018-10-31 19:36:31 +0100

    vendor/digi_sdk_addon: add api stubs jar
    This stubs library allows customers to build applications that use our APIs
    inside the Android sources.
    
    To do so, customers must add 'com.digi.android.stubs' as a java library in the
    application makefile:
    
      * Android.mk:
    
        LOCAL_JAVA_LIBRARIES := com.digi.android.stubs
    
      * Android.bp:
    
        java_libs: ["com.digi.android.stubs"],
    
    https://jira.digi.com/browse/ADK4A-1390
    
    Signed-off-by: Tatiana Leon <tatiana.leon@digi.com>

--- digi_sdk_addon/Android.mk
+++ digi_sdk_addon/Android.mk
@@ -26,6 +26,24 @@ LOCAL_SRC_FILES := $(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
 LOCAL_BUILT_MODULE_STEM := $(LOCAL_MODULE)$(COMMON_JAVA_PACKAGE_SUFFIX)
 include $(BUILD_PREBUILT)
 
+# Digi API stubs
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := com.digi.android.stubs
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_JAVA_LIBRARIES)
+LOCAL_MODULE_SUFFIX := $(COMMON_JAVA_PACKAGE_SUFFIX)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
+LOCAL_BUILT_MODULE_STEM := $(LOCAL_MODULE)$(COMMON_JAVA_PACKAGE_SUFFIX)
+stubs.intermediates := $(call local-intermediates-dir, COMMON)
+stubs.src_file := $(LOCAL_PATH)/$(LOCAL_MODULE)$(LOCAL_MODULE_SUFFIX)
+$(stubs.intermediates)/classes.dex.toc: $(stubs.src_file)
+	@mkdir -p $(dir $@)
+	$(hide) rm -f $(stubs.intermediates)/classes.jack
+	$(hide) cp $< $(stubs.intermediates)/classes.jack
+	$(hide) touch $@
+include $(BUILD_PREBUILT)
+
 # Copy com.digi.android.xml to /system/etc/permissions/
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
